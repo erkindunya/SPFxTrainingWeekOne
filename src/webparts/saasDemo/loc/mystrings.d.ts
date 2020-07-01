3739daf5badcc9c07cd031b2a5ca4d84c464250a
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-01 12:26:01 +0100

    Saas Demo web part v1

---
+++ src/webparts/saasDemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISaasDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SaasDemoWebPartStrings' {
+  const strings: ISaasDemoWebPartStrings;
+  export = strings;
+}
